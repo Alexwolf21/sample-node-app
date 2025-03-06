c5121497b2047469d5eab477e09f4b8e01708cef
Alexwolf21 <163804464+Alexwolf21@users.noreply.github.com>
2025-03-06 19:41:50 +0530

    Updated sample app for Prometheus

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Create a Registry and collect default metrics
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics({ register });
+
+// Define an endpoint for Prometheus to scrape metrics
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, World!This is a sample application");
 });
