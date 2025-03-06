1866b1a605975b5ef21a72c0fb80c4b98d5a6c48
Alexwolf21 <163804464+Alexwolf21@users.noreply.github.com>
2025-03-06 21:09:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
-const express = require("express");
-const promClient = require("prom-client");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-// Create a Registry and collect default metrics
-promClient.collectDefaultMetrics({ register });
-
-// Define a custom counter metric (optional)
-const requestCounter = new promClient.Counter({
-  name: "node_request_total",
-  help: "Total number of requests received",
-});
-
-// Middleware to count every request
-app.use((req, res, next) => {
-  requestCounter.inc();
-  next();
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!This is a sample application");
-});
-
-// Define an endpoint for Prometheus to scrape metrics
-app.get("/metrics", async (req, res) => {
-  try {
-    res.set("Content-Type", promClient.register.contentType);
-    res.end(await promClient.register.metrics());
-  } catch (ex) {
-    res.status(500).end(ex);
-  }
+app.get('/', (req, res) => {
+  res.send('Hello, World! This is the sample app.');
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`App is running on port ${port}`);
 });
