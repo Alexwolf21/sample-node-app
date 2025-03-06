ab0b5fbebdbec10ce7d5e017ca3f20d392afef72
Alexwolf21 <163804464+Alexwolf21@users.noreply.github.com>
2025-03-06 19:52:17 +0530

    Updated Code for Prometheus

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const express = require("express");
+const promClient = require("prom-client");
 const app = express();
 const port = 3000;
 
 // Create a Registry and collect default metrics
-const register = new promClient.Registry();
 promClient.collectDefaultMetrics({ register });
 
-// Define an endpoint for Prometheus to scrape metrics
-app.get("/metrics", async (req, res) => {
-  res.set("Content-Type", register.contentType);
-  res.end(await register.metrics());
+// Define a custom counter metric (optional)
+const requestCounter = new promClient.Counter({
+  name: "node_request_total",
+  help: "Total number of requests received",
+});
+
+// Middleware to count every request
+app.use((req, res, next) => {
+  requestCounter.inc();
+  next();
 });
 
 app.get("/", (req, res) => {
   res.send("Hello, World!This is a sample application");
+});
+
+// Define an endpoint for Prometheus to scrape metrics
+app.get("/metrics", async (req, res) => {
+  try {
+    res.set("Content-Type", promClient.register.contentType);
+    res.end(await promClient.register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
 });
 
 app.listen(port, () => {
