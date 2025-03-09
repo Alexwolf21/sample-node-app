92686e528d388305168fe35de2a917bd8ebaadee
Alexwolf21 <163804464+Alexwolf21@users.noreply.github.com>
2025-03-10 01:17:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const promClient = require("prom-client");
 const app = express();
-const port = 8088;
+const port = 3000;
 
 // Collect default metrics (e.g., process and OS metrics)
 promClient.collectDefaultMetrics();
